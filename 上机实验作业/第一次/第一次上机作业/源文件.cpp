67823cd0058c0c05b4c7095142028ed5a0507462
Rosist Sallina <2779477248@qq.com>
2023-11-07 22:26:12 +0800

    Add files via upload

---
+++ 上机实验作业/第一次/第一次上机作业/源文件.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::cout << "刘宇轩" << std::endl << "2023010892" << std::endl
+		<< "计算机系" << std::endl << "计38-经32" << std::endl;
+
+	printf("刘宇轩\n");
+	printf("2023010892\n");
+	printf("计算机系\n");
+	printf("计38-经32\n");
+
+	return 0;
+}
